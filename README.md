f2475b092b7be8ab00b85f9acb52f03133577348
许海菁 <foreverxhj@gmail.com>
2019-12-09 15:00:04 +0800

    新增模板引擎中间件

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: xuhj
  * @Date: 2019-12-04 17:37:09
- * @LastEditTime: 2019-12-09 14:30:33
+ * @LastEditTime: 2019-12-09 14:59:45
  * @Description: 
  -->
 # 基于Koa的豆瓣预告片网站
@@ -9,3 +9,5 @@
 ### 新增动态模板
 ## daily-5-4
 ### 新增 pug  ejs 模板引擎 
+## daily-5-5
+### 新增模板引擎中间件
--- server/index.js
+++ server/index.js
@@ -1,22 +1,27 @@
 /*
  * @Author: xuhj
  * @Date: 2019-12-04 17:40:29
- * @LastEditTime: 2019-12-09 14:21:19
+ * @LastEditTime: 2019-12-09 14:47:03
  * @Description: 
  */
 const Koa = require( 'koa' )
 const app = new Koa()
-const { htmlTpl, ejsTpl, pugTpl } = require( './templete' )
-const ejs = require( 'ejs' )
-const pug = require('pug')
+// const { htmlTpl, ejsTpl, pugTpl } = require( './templete' )
+// const ejs = require( 'ejs' )
+// const pug = require('pug')
+const views = require( 'koa-views' )
+const { resolve } = require( 'path' )
 
-app.use( ( async ( ctx, next ) => {
-    ctx.type = 'text/html; charset=utf-8 '
-    ctx.body = pug.render( pugTpl, {
-        you: 'Luke!',
+app.use( views( resolve( __dirname, './views' ), {
+    extension: 'pug'
+}))
+
+app.use( async ( ctx, next ) => {
+    await ctx.render( 'index', {
+        you: 'Luke!!',
         me: 'Scott'
     })
-} ) )
+})
 
 app.listen( 3000 )
 console.log('server is start up')
